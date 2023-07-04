eb82c779093ae59d9e72cd60a28ecfaba2dcd366
Andy Albrecht <aalbrecht33733@gmail.com>
2023-07-04 19:36:22 -0400

    Capstone Java project - ELift

---
+++ src/main/java/com/capstone/eLift/ELiftApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.eLift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELiftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capstone/eLift/ELiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.eLift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
